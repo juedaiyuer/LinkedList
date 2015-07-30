dc5a16cec0cecfb61abf1d3dceb6e1b634565e7e
juedaiyuer <337189277@qq.com>
2015-07-30 16:20:27 +0800

    Revert "Revert "only delete operate not work""
    This reverts commit 9e8e57139d6869c0c14028a8e1e5048e2a3945dd.

--- List2.cpp
+++ List2.cpp
@@ -2,39 +2,77 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct _list{
-	Node* head;
-} List;
+/**typedef struct _node{
+	int value;
+	struct _node *next; 
+} Node;
+*/
 
-void add( Node* head, int number);
+Node* add(Node*head,int number);
+void print(Node *head);
+Node* delete_number(Node *head,int number);  //delete node of linkedList is number 
 
 int main(int argc, const char* argv[]){
-	List list;
+	Node *head;
 	int number;
-	list.head = NULL;
+	head=NULL;
 	do{
 		scanf("%d",&number);
 		if(number != -1){ 
-		add(&list, number);
+		head = add(head, number);			
 		}
+       // print(head);       //intersting test and go it
 	}while(number != -1);
+	
+	print(head);    // test output
+
+    int num;
+	scanf("%d",&num);   //test delete operate 
+    head = delete_number(head,num);
+	print(head);
 
 	return 0;
 }
 
-void add(List* pList,int number)
+Node* add(Node*head,int number)
 {
 	Node *p = (Node*)malloc(sizeof(Node));
 	p->value = number;
 	p->next = NULL;
-	Node *last = pList->head;
+	Node *last = head;
 	if(last){
 		while(last->next){
 			last = last->next;
 		}
 		last->next = p;
 	}else{
-		pList ->head = p;
+		head = p;
 	}
 	
+	return head;
+}
+
+void print(Node *head){
+	Node *p;
+	for(p=head;p;p=p->next){
+		printf("%d\t",p->value);
+	}
+	printf("\n");
+}
+Node* delete_number(Node *head,int number){
+	Node *q;
+	Node *p;
+	for(q=NULL,p=head;p;q=p,p->next){
+		if(p->value == number){
+			if(q){
+				q->next = p->next;
+			}else{
+				head = p->next;
+			}
+			free(p);
+
+			break;
+		}
+	}
+	return head;
 }
