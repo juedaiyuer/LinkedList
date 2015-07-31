942f8f35fc5e4a893b8d421cbff9a2447e07f256
juedaiyuer <337189277@qq.com>
2015-07-31 10:22:06 +0800

    SingleLinkedList
    单向链表的最终版本

---
+++ SingleLinkedList.cpp
@@ -0,0 +1,91 @@
+#include"node.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**typedef struct _node{
+	int value;
+	struct _node *next; 
+} Node;
+*/
+
+Node* add(Node*head,int number);
+void print(Node *head);
+Node* delete_number(Node *head,int number);  //delete node of linkedList is number 
+void delete_list(Node *head);
+
+int main(int argc, const char* argv[]){
+	Node *head;
+	int number;
+	head=NULL;
+	do{
+		scanf("%d",&number);
+		if(number != -1){ 
+		head = add(head, number);			
+		}
+       // print(head);       //intersting test and go it
+	}while(number != -1);
+	
+	print(head);    // test output
+
+    int num;
+	scanf("%d",&num);   //test delete operate 
+    head = delete_number(head,num);
+	print(head);
+
+    delete_list(head);
+	print(head);
+
+	return 0;
+}
+
+Node* add(Node*head,int number)
+{
+	Node *p = (Node*)malloc(sizeof(Node));
+	p->value = number;
+	p->next = NULL;
+	Node *last = head;
+	if(last){
+		while(last->next){
+			last = last->next;
+		}
+		last->next = p;
+	}else{
+		head = p;
+	}
+	
+	return head;
+}
+
+void print(Node *head){
+	Node *p;
+	for(p=head;p;p=p->next){
+		printf("%d\t",p->value);
+	}
+	printf("\n");
+}
+Node* delete_number(Node *head,int number){
+	Node *q;
+	Node *p;
+	for(q=NULL,p=head;p;q=p,p=p->next){
+		if(p->value == number){
+			if(q){
+				q->next = p->next;
+			}else{
+				head = p->next;
+			}
+			free(p);
+
+			break;
+		}
+	}
+	return head;
+}
+
+void delete_list(Node *head){
+	Node *p;
+	Node *q;
+	for(p=head;p;p=q){
+		q = p->next;
+		free(p);
+	}
+}
